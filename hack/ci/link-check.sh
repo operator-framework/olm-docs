b72802291720656b7eccd8c6df648cfa8c706b3e
timflannagan <timflannagan@gmail.com>
2021-02-09 10:53:10 -0500

    hack/ci: Support passing additional run options to a container engine
    Update the `hack/ci/lint-check.sh` bash script, which is responsible for
    linting the general HTML output code, and expose the ability to set
    additional run options to docker. This is nice in the case where you
    have selinux bits present in the cloned/forked repository locally, and
    that results in the inability to properly read from the mounted volume
    that contains the website source.

--- hack/ci/link-check.sh
+++ hack/ci/link-check.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -ev
 
+CONTAINER_RUN_EXTRA_OPTIONS=${CONTAINER_RUN_EXTRA_OPTIONS:=""}
+
 docker volume create olm-html
-docker run --rm -v "$(pwd):/src" -v olm-html:/src/public klakegg/hugo:0.73.0-ext-ubuntu
+docker run --rm ${CONTAINER_RUN_EXTRA_OPTIONS} -v "$(pwd):/src" -v olm-html:/src/public klakegg/hugo:0.73.0-ext-ubuntu
 docker run --rm -v olm-html:/target mtlynch/htmlproofer /target --empty-alt-ignore --http-status-ignore 429 --allow_hash_href
 docker volume rm olm-html
