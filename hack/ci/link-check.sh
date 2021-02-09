3e350b964898317cb17102f24f233d8f4ff44f61
timflannagan <timflannagan@gmail.com>
2021-02-09 11:49:17 -0500

    hack/ci: Support overriding the container engine in the linting script
    Support overriding the default container engine (docker) with podman
    using the `$CONTAINER_ENGINE` environment variable.

--- hack/ci/link-check.sh
+++ hack/ci/link-check.sh
@@ -2,8 +2,10 @@
 set -ev
 
 CONTAINER_RUN_EXTRA_OPTIONS=${CONTAINER_RUN_EXTRA_OPTIONS:=""}
+CONTAINER_ENGINE=${CONTAINER_ENGINE:="docker"}
 
-docker volume create olm-html
-docker run --rm ${CONTAINER_RUN_EXTRA_OPTIONS} -v "$(pwd):/src" -v olm-html:/src/public klakegg/hugo:0.73.0-ext-ubuntu
-docker run --rm -v olm-html:/target mtlynch/htmlproofer /target --empty-alt-ignore --http-status-ignore 429 --allow_hash_href
-docker volume rm olm-html
+# TODO(tflannag): We may need to trap that `... volume rm` call.
+${CONTAINER_ENGINE} volume create olm-html
+${CONTAINER_ENGINE} run --rm ${CONTAINER_RUN_EXTRA_OPTIONS} -v "$(pwd):/src" -v olm-html:/src/public klakegg/hugo:0.73.0-ext-ubuntu
+${CONTAINER_ENGINE} run --rm -v olm-html:/target mtlynch/htmlproofer /target --empty-alt-ignore --http-status-ignore 429 --allow_hash_href
+${CONTAINER_ENGINE} volume rm olm-html
